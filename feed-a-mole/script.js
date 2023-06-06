06228ff7b733c98d3ef6dda02e52615a7a36d6dd
Andrew Fiasco <affiasco@umich.edu>
2023-06-06 09:34:40 -0400

    Add winning function in js and image

--- feed-a-mole/script.js
+++ feed-a-mole/script.js
@@ -1,6 +1,8 @@
-const moles = document.querySelectorAll(".mole");
+const container = document.querySelector(".container");
+const moleHoles = document.querySelector(".mole-holes");
 const mole = document.querySelector(".mole");
 const body = document.querySelector("body");
+const worm = document.querySelector(".worm");
 
 let runningScore = 0;
 
@@ -14,18 +16,23 @@ function cursorWormUpdateScore(classList) {
   if (classList === "mole") {
     body.style.cursor = "url(./static/mole-game/cursor-worm.png), auto";
     runningScore += 1;
-    increaseWorm();
+    worm.style.maxWidth = `${runningScore}0%`;
   }
 
   setTimeout(
     () => (body.style.cursor = "url(./static/mole-game/cursor.png), auto"),
     1000
   );
+  showWinner(runningScore);
 }
 
-function increaseWorm() {
-  const worm = document.querySelector(".worm");
-  worm.style.maxWidth = `${runningScore}0%`;
+function showWinner(runningScore) {
+  if (runningScore === 10) {
+    const winImg = document.createElement("img");
+    winImg.src = "./static/mole-game/win.png";
+    winImg.classList.add("winner");
+    body.replaceChild(winImg, container);
+  }
 }
 
 // Used for Timing //

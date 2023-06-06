9b4f24a3fa89fd3852b19f62608150633d7cdb80
Andrew Fiasco <affiasco@umich.edu>
2023-06-05 21:33:01 -0400

    Add cursor click change, comment out timing for now

--- feed-a-mole/script.js
+++ feed-a-mole/script.js
@@ -1,26 +1,38 @@
 const moles = document.querySelectorAll(".mole");
 const mole = document.querySelector(".mole");
+const body = document.querySelector("body");
 
-let isHungry = true;
-let nextTime = Date.now();
-
-function next() {
-  if (Date.now() > nextTime) {
-    mole.src = isHungry
-      ? "./static/mole-game/mole-sad.png"
-      : "./static/mole-game/mole-hungry.png";
-    isHungry = !isHungry;
-    nextTime = Date.now() + 1000;
-  }
-  requestAnimationFrame(next);
+function changeWorm() {
+  document.addEventListener("click", (event) =>
+    cursorWorm(event.target.classList[0])
+  );
 }
-next();
 
-// function rafCounter() {
-//   if (Date.now() > runAgainAt) {
-//     rafCount++;
-//     runAgainAt = Date.now() + 1000;
+function cursorWorm(classList) {
+  if (classList === "mole") {
+    body.style.cursor = "url(./static/mole-game/cursor-worm.png), auto";
+  }
+
+  setTimeout(
+    () => (body.style.cursor = "url(./static/mole-game/cursor.png), auto"),
+    1000
+  );
+}
+
+// Used for Timing //
+// let isHungry = true;
+// let nextTime = Date.now();
+
+// function next() {
+//   if (Date.now() > nextTime) {
+//     mole.src = isHungry
+//       ? "./static/mole-game/mole-sad.png"
+//       : "./static/mole-game/mole-hungry.png";
+//     isHungry = !isHungry;
+//     nextTime = Date.now() + 1000;
 //   }
-//   requestAnimationFrame(rafCounter);
+//   requestAnimationFrame(next);
 // }
-// requestAnimationFrame(rafCounter);
+// next();
+
+changeWorm();

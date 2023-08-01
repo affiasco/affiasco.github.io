47d3d619fb747b119a4836446f4ac9f4c2296611
Andrew Fiasco <affiasco@umich.edu>
2023-08-01 12:28:31 -0400

    Adjust  showBooksFromLibrary function, add basic styling of book cards

--- library/library.js
+++ library/library.js
@@ -10,9 +10,9 @@ class Book {
   }
 
   info() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${
-      this.read ? "read" : "not read"
-    }`;
+    return `<p>${this.title}</p> <p>${this.author} </p> <p> ${
+      this.pages
+    } pages </p> <p> ${this.read ? "read" : "not read"}</p>`;
   }
 }
 
@@ -23,8 +23,14 @@ function addBookToLibrary(bookObj) {
 function showBooksFromLibrary(myLibrary) {
   myLibrary.forEach((book) => {
     const newBook = document.createElement("div");
+    const bookInfo = document.createElement("div");
+    const bookDetails = document.createElement("p");
+    newBook.classList.add("book-card");
+    bookInfo.classList.add("book-info");
+    bookDetails.classList.add("book-details");
     myLibraryOnPage.appendChild(newBook);
-    newBook.innerText = book.info();
+    newBook.appendChild(bookInfo);
+    bookInfo.innerHTML = book.info();
   });
 }
 
@@ -32,10 +38,14 @@ function showBooksFromLibrary(myLibrary) {
 faket = new Book("fakeT", "FakeA", 36, true);
 faket2 = new Book("fakeT2", "FakeA2", 100, false);
 faket3 = new Book("fakeT3", "FakeA3", 36, true);
+faket4 = new Book("fakeT4", "FakeA4", 36, true);
+faket5 = new Book("fakeT5", "FakeA5", 36, true);
 
 addBookToLibrary(faket);
 addBookToLibrary(faket2);
 addBookToLibrary(faket3);
+addBookToLibrary(faket4);
+addBookToLibrary(faket5);
 // --- end ---
 
 showBooksFromLibrary(myLibrary);

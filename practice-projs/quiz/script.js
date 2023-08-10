bf074f817f4ca16ea48871be0e33e11fd8985c39
Andrew <affiasco@umich.edu>
2023-08-10 19:05:10 -0400

    refactor JS file into module

--- practice-projs/quiz/script.js
+++ practice-projs/quiz/script.js
@@ -1,50 +1,58 @@
-const highScoreButton = document.querySelector(".high-score-button");
+const Quiz = () => {
+  const message = document.querySelector(".message");
+  const timer = document.querySelector(".timer");
+  let time = 120;
 
-highScoreButton.addEventListener("click", (event) => {
-  console.log(event.target);
-})
+  const renderTimer = () => {
+    timer.innerText = `Timer: ${time}`;
+  };
+
+  const startTimer = () => {
+    setInterval(() => {
+      time --;
+      timer.innerText = `Timer: ${time}`;
+    }, 1000)
+  }
+
+  const startQuiz = (startQuizSection, questionSection) => {
+    startQuizSection.classList.add("hidden");
+    questionSection.classList.remove("hidden");
+    message.innerText = "";
+    startTimer();
+  };
+
+  const answerResponse = event => {
+    const isIncorrect = event.target.classList.contains("ncor");
+    const isCorrect = event.target.classList.contains("cor");
+
+    message.innerText = isIncorrect ? "Incorrect :(" : (isCorrect ? "Correct!" : "");
+    message.classList.toggle("incorrect", isIncorrect);
+    message.classList.toggle("correct", isCorrect);
+
+    if (isIncorrect) {
+      event.target.classList.add("incorrect-answer");
+      time -= 10;
+      setTimeout(() => {
+        event.target.classList.remove("incorrect-answer");
+      }, 500);
+    }
+  };
+
+
+
+  return { answerResponse, renderTimer, startTimer, startQuiz };
+};
+
+const quizModule = Quiz();
+quizModule.renderTimer();
 
 const startButton  = document.querySelector(".start-button");
 const startQuizSection = document.querySelector(".start-quiz-section")
-
 const questionSection = document.querySelector(".question-section")
-const message = document.querySelector(".message")
 
-startButton.addEventListener("click", (event) => {
-  console.log(event.target);
-  startQuizSection.classList.add("hidden");
-  questionSection.classList.remove("hidden")
-  message.innerText = ""
-  startTimer();
-})
+startButton.addEventListener("click", () => {
+  quizModule.startQuiz(startQuizSection, questionSection);
+});
 
-let time = 120;
-const timer = document.querySelector(".timer");
-timer.innerText = `Timer: ${time}`;
-
-// question buttons
-questionSection.addEventListener("click", (event) => {
-  message.innerText = "";
-  if (event.target.classList.contains("ncor")) {
-    event.target.style.border = "2px solid red"
-    event.target.style.color = "red"
-    message.innerText = "Incorrect :("
-    time -= 10;
-    setTimeout(() => {
-      event.target.style.border = "2px solid #218380"
-      event.target.style.color = "white"
-    } , 500)
-  } 
-
-  if (event.target.classList.contains("cor")) {
-    message.innerText = "Correct!"
-  }
-})
-
-// time
-const startTimer = () => {
-  setInterval(() => {
-    time --;
-    timer.innerText = `Timer: ${time}`;
-  }, 1000)
-}
+const buttonQuestion = document.querySelectorAll(".button-question")
+buttonQuestion.forEach((button) => button.addEventListener("click", quizModule.answerResponse))

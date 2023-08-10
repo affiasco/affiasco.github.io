bc6a75531fcf6f35832580db01ed298a67f59150
Andrew <affiasco@umich.edu>
2023-08-10 00:21:42 -0400

    Add first question and some basic JS

--- practice-projs/quiz/script.js
+++ practice-projs/quiz/script.js
@@ -1,13 +1,39 @@
 const highScoreButton = document.querySelector(".high-score-button");
 
 highScoreButton.addEventListener("click", (event) => {
-  console.log(event.target)
+  console.log(event.target);
 })
 
 const startButton  = document.querySelector(".start-button");
 const startQuizSection = document.querySelector(".start-quiz-section")
 
+const questionSection = document.querySelector(".question-section")
+const message = document.querySelector(".message")
+
 startButton.addEventListener("click", (event) => {
-  console.log(event.target)
+  console.log(event.target);
   startQuizSection.classList.add("hidden");
+  questionSection.classList.remove("hidden")
+  message.innerText = ""
+})
+
+
+// question buttons
+questionSection.addEventListener("click", (event) => {
+  message.innerText = "";
+  if (event.target.classList.contains("ncor")) {
+    event.target.style.border = "2px solid red"
+    event.target.style.color = "red"
+    message.innerText = "Incorrect :("
+    setTimeout(() => {
+      event.target.style.border = "2px solid #218380"
+      event.target.style.color = "white"
+    } , 500)
+  } 
+
+  if (event.target.classList.contains("cor")) {
+    message.innerText = "Correct!"
+  }
+
+ 
 })

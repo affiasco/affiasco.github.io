c130314f87c632b69dbfbdfc11b6cc6ec456de92
Andrew Fiasco <affiasco@umich.edu>
2023-08-12 14:46:00 -0400

    Add js functionality and HTML to show resuls after completing quiz

--- practice-projs/quiz/script.js
+++ practice-projs/quiz/script.js
@@ -1,7 +1,7 @@
 const Quiz = () => {
   const message = document.querySelector(".message");
   const timer = document.querySelector(".timer");
-  let time = 120;
+  let time = 90;
   let score = 0;
 
   const renderTimer = () => {
@@ -12,21 +12,25 @@ const Quiz = () => {
     setInterval(() => {
       time--;
       timer.innerText = `Timer: ${time}`;
-    }, 1000)
-  }
+    }, 1000);
+  };
 
   const startQuiz = (startQuizSection, questionSection) => {
     startQuizSection.classList.add("hidden");
     questionSection.classList.remove("hidden");
     message.innerText = "";
-          startTimer();
+    startTimer();
   };
 
-  const answerResponse = event => {
+  const answerResponse = (event) => {
     const isIncorrect = event.target.classList.contains("ncor");
     const isCorrect = event.target.classList.contains("cor");
 
-    message.innerText = isIncorrect ? "Incorrect :(" : (isCorrect ? "Correct!" : "");
+    message.innerText = isIncorrect
+      ? "Incorrect :("
+      : isCorrect
+      ? "Correct!"
+      : "";
     message.classList.toggle("incorrect", isIncorrect);
     message.classList.toggle("correct", isCorrect);
 
@@ -40,49 +44,80 @@ const Quiz = () => {
 
     if (isCorrect) {
       score += 10;
-      showNextQuestion(event.target)
-    };
+      showNextQuestion(event.target);
+    }
   };
 
-  const showNextQuestion = eventTarget => {
+  const showNextQuestion = (eventTarget) => {
     const questionNum = eventTarget.parentElement.dataset.question;
     setTimeout(() => {
       message.innerText = "";
     }, 1000);
-      
-    if (questionNum >= 5) {
-      // showFinalResults();
-      return;
-    }
-  
-    const nextQuestionNum = parseInt(questionNum) + 1;
-    const currentQuestion = document.querySelector(`[data-question="${questionNum}"]`);
-    const nextQuestion = document.querySelector(`[data-question="${nextQuestionNum}"]`);
-  
+
+    const currentQuestion = document.querySelector(
+      `[data-question="${questionNum}"]`
+    );
+
     currentQuestion.classList.add("hidden");
-    nextQuestion.classList.remove("hidden");
+    const nextQuestionNum = parseInt(questionNum) + 1;
+    const nextQuestion = document.querySelector(
+      `[data-question="${nextQuestionNum}"]`
+    );
+
+    nextQuestionNum > 5
+      ? showFinalResultSection()
+      : nextQuestion.classList.remove("hidden");
   };
-
-  // showFinalResult(); {
-      // also will want to render the final results of the timer is 0
-      // and hide the current question
-
-  // }
 
   const getScore = () => score;
 
-  return { answerResponse, renderTimer, startTimer, startQuiz, getScore };
+  const renderFinalScore = () => {
+    const scoreEle = document.querySelector(".show-final-score");
+    scoreEle.innerText = `Your Final Score is: ${getScore()}`;
+  };
+
+  const stopTimer = () => {
+    const currentTime = timer.innerText;
+    return currentTime.split(":")[1];
+  };
+
+  const renderFinalTime = () => {
+    const finalTime = parseInt(stopTimer());
+    console.log(finalTime);
+    const showFinalTime = document.querySelector(".show-final-time");
+    showFinalTime.innerText = `You completed the quiz in: ${
+      90 - finalTime
+    } seconds`;
+  };
+
+  const showFinalResultSection = () => {
+    const finalScoreSection = document.querySelector(".final-score-section");
+    finalScoreSection.classList.remove("hidden");
+    renderFinalScore();
+    renderFinalTime();
+  };
+
+  return {
+    answerResponse,
+    renderTimer,
+    startTimer,
+    startQuiz,
+    getScore,
+  };
 };
 
 const quizModule = Quiz();
 quizModule.renderTimer();
 
 const startButton = document.querySelector(".start-button");
-const startQuizSection = document.querySelector(".start-quiz-section")
-const questionSection = document.querySelector(".question-section")
+const startQuizSection = document.querySelector(".start-quiz-section");
+const questionSection = document.querySelector(".question-section");
 
 startButton.addEventListener("click", () =>
-  quizModule.startQuiz(startQuizSection, questionSection));
+  quizModule.startQuiz(startQuizSection, questionSection)
+);
 
-const buttonQuestion = document.querySelectorAll(".button-question")
-buttonQuestion.forEach((button) => button.addEventListener("click", quizModule.answerResponse));   
+const buttonQuestion = document.querySelectorAll(".button-question");
+buttonQuestion.forEach((button) =>
+  button.addEventListener("click", quizModule.answerResponse)
+);

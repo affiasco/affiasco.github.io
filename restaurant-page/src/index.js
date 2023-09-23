d1a69dd5a331bbe3b55833261d623759bfdcdacd
Andrew Fiasco <affiasco@umich.edu>
2023-09-22 23:02:38 -0400

    Create menu.js module structure

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,5 +1,7 @@
 import { renderNav } from "./nav";
 import { renderHomepage } from "./home";
+import { renderMenuPage } from "./menu";
 
 renderNav();
 // renderHomepage();
+renderMenuPage();
--- restaurant-page/src/menu.js
+++ restaurant-page/src/menu.js
@@ -1,7 +1,32 @@
 const MenuPage = () => {
   const content = document.querySelector(".content");
 
-  const menuPageMainContent = () => {};
+  const menuPageMainContent = () => {
+    const container = document.createElement("div");
+    container.classList.add("menu-container");
+    container.classList.add("content-container");
 
-  return menuPageMainContent;
+    content.appendChild(container);
+    container.appendChild(beveragesSection());
+    container.appendChild(sidesSection());
+    container.appendChild(dishesSection());
+  };
+
+  const beveragesHeading = () => {};
+  const beveragesContent = () => {};
+  const beveragesSection = () => {};
+
+  const sidesHeading = () => {};
+  const sidesContent = () => {};
+  const sidesSection = () => {};
+
+  const dishesHeading = () => {};
+  const dishesContent = () => {};
+  const dishesSection = () => {};
+
+  return { menuPageMainContent };
 };
+
+export function renderMenuPage() {
+  return MenuPage().menuPageMainContent();
+}

abcecc1bdc57db1e6404182486cb926d5cd64d10
Andrew Fiasco <affiasco@umich.edu>
2023-09-23 09:49:24 -0400

    Add contact heading and chef contact function

--- restaurant-page/src/contact.js
+++ restaurant-page/src/contact.js
@@ -10,9 +10,39 @@ const ContactPage = () => {
     container.appendChild(contactSection());
   };
 
-  const contactHeading = () => {};
-  const contactContent = () => {};
-  const contactSection = () => {};
+  const contactHeading = () => {
+    const heading = document.createElement("h1");
+    heading.innerText = "Contact Us";
+
+    return heading;
+  };
+
+  const contactChef = () => {
+    const chefSection = document.createElement("div");
+    chefSection.classList.add("contact-chef");
+
+    const chefName = document.createElement("h2");
+    chefName.innerText = "Chef Name";
+
+    const chefInfo = document.createElement("p");
+    chefInfo.innerText = `Name</br>123-456-7890</br>email@email.com`;
+
+    chefSection.appendChild(chefName);
+    chefSection.appendChild(chefInfo);
+
+    return chefSection;
+  };
+
+  const contactContent = () => {
+    const contactContent = document.createElement("div");
+
+    contactContent.appendChild(contactChef());
+  };
+  const contactSection = () => {
+    const contentSection = document.createElement("div");
+    contentSection.appendChild(contactHeading());
+    contentSection.appendChild(contactContent());
+  };
 
   return { contactPageMainContent };
 };

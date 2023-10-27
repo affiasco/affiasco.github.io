f9f03305430b2f038e7f52cab17af02f2fac5eac
Andrew <affiasco@umich.edu>
2023-10-26 23:42:41 -0400

    add main-content-heading area and include add-task button, applying styling and structure

--- todo-list/src/renderDomContent.js
+++ todo-list/src/renderDomContent.js
@@ -2,23 +2,16 @@ const renderDomContent = () => {
   const mainContentContainer = document.querySelector(".main-content");
 
   const addTaskHeader = (headerText) => {
+    const headingContainer = document.createElement("div");
+    headingContainer.classList.add("heading-container");
+
     const newHeader = document.createElement("h1");
     newHeader.classList.add("task-header");
-
-    mainContentContainer.append(newHeader);
     newHeader.innerText = headerText;
-  };
 
-  const showCorrectPage = (infoObj) => {
-    const sideNavBtn = document.querySelector(infoObj.className);
-
-    sideNavBtn.addEventListener("click", () => renderPage(infoObj));
-    addRemoveActive();
-  };
-
-  const renderPage = (infoObj) => {
-    mainContentContainer.innerHTML = "";
-    addTaskHeader(infoObj.headerText);
+    mainContentContainer.appendChild(headingContainer);
+    headingContainer.appendChild(addTaskButton());
+    headingContainer.appendChild(newHeader);
   };
 
   const addRemoveActive = () => {
@@ -33,6 +26,31 @@ const renderDomContent = () => {
         e.target.classList.add("active");
       });
     });
+  };
+
+  const addTaskButton = () => {
+    const taskButtonContainer = document.createElement("div");
+    taskButtonContainer.classList.add("task-btn-container");
+
+    const taskButton = document.createElement("button");
+    taskButton.classList.add("add-task-btn");
+    taskButton.innerText = "Add Task";
+
+    taskButtonContainer.appendChild(taskButton);
+
+    return taskButtonContainer;
+  };
+
+  const renderPage = (infoObj) => {
+    mainContentContainer.innerHTML = "";
+    addTaskHeader(infoObj.headerText);
+  };
+
+  const showCorrectPage = (infoObj) => {
+    const sideNavBtn = document.querySelector(infoObj.className);
+
+    sideNavBtn.addEventListener("click", () => renderPage(infoObj));
+    addRemoveActive();
   };
 
   return { showCorrectPage };
